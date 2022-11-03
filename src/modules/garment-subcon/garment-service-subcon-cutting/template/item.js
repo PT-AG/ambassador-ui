3f297d7e27b8595df64fff8dec7f18ebb60ae79c
yokana888 <yokana.adita@outlook.com>
2022-11-03 13:10:29 +0700

    enhance filter CuttingInNo PL Subcon Jasa Komponen

--- src/modules/garment-subcon/garment-service-subcon-cutting/template/item.js
+++ src/modules/garment-subcon/garment-service-subcon-cutting/template/item.js
@@ -7,6 +7,8 @@ const CuttingInLoader = require('../../../../loader/garment-cutting-in-by-ro-loa
 export class Item {
     @bindable selectedCuttingIn;
 
+    ROList = [];
+
     constructor(service, salesService, coreService) {
         this.service = service;
         this.salesService = salesService;
@@ -40,6 +42,10 @@ export class Item {
         this.isCreate = context.context.options.isCreate;
         this.isEdit = context.context.options.isEdit;
         this.itemOptions=context.context.options;
+        this.ROList = this.itemOptions.ROList;
+
+        console.log(this.itemOptions);
+
         if(this.data){
             this.selectedCuttingIn={
                 RONo:this.data.RONo,
@@ -53,6 +59,7 @@ export class Item {
             }
         }
     }
+
     itemsColumnsCreate= [
            // "Kode Barang",
             "Keterangan",
@@ -79,18 +86,44 @@ export class Item {
         return `${comodity.Code} - ${comodity.Name}`
     }
 
+    // get cuttingInLoader() {
+    //     return CuttingInLoader;
+    // }
+
     get cuttingInLoader() {
-        return CuttingInLoader;
+        return async (keyword) => {
+			var info = {
+				keyword: keyword,
+				filter: this.data.Unit && this.data.Buyer ? 
+                        JSON.stringify({ 
+                            UnitId: this.data.Unit.Id,
+                            CuttingFrom:"PREPARING",
+                            CuttingType:"MAIN FABRIC",
+                            BuyerCode: this.data.Buyer.Code
+                        }) : JSON.stringify({ 
+                            UnitId: 0,
+                            CuttingFrom:"PREPARING",
+                            CuttingType:"MAIN FABRIC",
+                            BuyerCode: ""
+                        }),
+                size : 10
+			};
+
+			return this.service.getCuttingInByROLoader(info).then((result) => {
+                console.log(result.data);
+                return result.data.filter(x => !this.ROList.includes(x.RONo));
+            });
+        }
     }
 
     async selectedCuttingInChanged(newValue, oldValue){
         if(this.isCreate){
             if(newValue) {
-                console.log(newValue)
                 if(this.data.Details.length>0){
                     this.data.Details.splice(0);
                 }
                 //this.context.error.Items = [];
+                this.ROList.push(newValue.RONo);
                 this.data.RONo = newValue.RONo;
                 this.data.Article = newValue.Article;
                 let noResult = await this.salesService.getCostCalculationByRONo({ size: 1, filter: JSON.stringify({ RO_Number: this.data.RONo }) });
@@ -125,8 +158,9 @@ export class Item {
                     }
                 }
                 //console.log()
-                Promise.resolve(this.service.getCuttingIn({ filter: JSON.stringify({ RONo: this.data.RONo, UnitId: this.data.Unit.Id, CuttingType:"MAIN FABRIC"}) }))
+                Promise.resolve(this.service.getCuttingIn({ filter: JSON.stringify({ RONo: this.data.RONo, UnitId: this.data.Unit.Id, CuttingType: "MAIN FABRIC"}) }))
                     .then(result => {
+                        console.log(result.data);
                         for(var cuttingInHeader of result.data){
                             for(var cuttingInItem of cuttingInHeader.Items){
                                 for(var cuttingInDetail of cuttingInItem.Details){
@@ -136,7 +170,6 @@ export class Item {
                                     if(ssCuttingItems[cuttingInDetail.Id]){
                                         qtyOut+=ssCuttingItems[cuttingInDetail.Id].qty;
                                     }
-                                    console.log(qtyOut)
                                    // if(cuttingInDetail.CuttingInQuantity-qtyOut>0){
                                         // cuttingInDetail.CuttingInId = cuttingInHeader.Id;
                                         // cuttingInDetail.CuttingInDetailId = cuttingInDetail.Id;
@@ -162,7 +195,9 @@ export class Item {
                                             exist.CuttingInQuantity+=cuttingInDetail.CuttingInQuantity-qtyOut;
                                             this.data.Details[idx]=exist;
                                         }
-                                    }    
+                                    }  
+                                    
+
                                    // }
                                     
                                 }
@@ -171,7 +206,13 @@ export class Item {
                     });
             }
             else {
+
                 this.context.selectedCuttingInViewModel.editorValue = "";
+
+                if(this.ROList.includes(this.data.RONo)) {
+                    this.ROList.splice(this.ROList.indexOf(this.data.RONo), 1);
+                }
+
                 this.data.RONo = null;
                 this.data.Article = null;
                 this.data.Comodity = null;
