12d4a64edca2749ecfa65e9fb5244fe588a3ddff
danliris <danliris.cloud@gmail.com>
2023-05-23 14:50:02 +0700

    Revert "BugFix-TotalCartoon-in-PackingListItems"

--- src/modules/garment-production/packing-list-items/template/detail-ro.js
+++ src/modules/garment-production/packing-list-items/template/detail-ro.js
@@ -374,24 +374,23 @@ export class Item {
           index: d.index
         };
       }).filter((value, i, self) => self.findIndex(f => value.carton1 == f.carton1 && value.carton2 == f.carton2 && value.index == f.index) === i);
-      console.log("detail",newDetails);
       for (var detail of newDetails) {
-        // const cartonExist = false;
-        // const indexItem = this.context.context.options.header.items.indexOf(this.data);
-        // if (indexItem > 0) {
-        //   for (let i = 0; i < indexItem; i++) {
-        //     const item = this.context.context.options.header.items[i];
-        //     for (const prevDetail of item.details) {
-        //       if (detail.carton1 == prevDetail.carton1 && detail.carton2 == prevDetail.carton2 && detail.index == prevDetail.index) {
-        //         cartonExist = true;
-        //         break;
-        //       }
-        //     }
-        //   }
-        // }
-        // if (!cartonExist) {
+        const cartonExist = false;
+        const indexItem = this.context.context.options.header.items.indexOf(this.data);
+        if (indexItem > 0) {
+          for (let i = 0; i < indexItem; i++) {
+            const item = this.context.context.options.header.items[i];
+            for (const prevDetail of item.details) {
+              if (detail.carton1 == prevDetail.carton1 && detail.carton2 == prevDetail.carton2 && detail.index == prevDetail.index) {
+                cartonExist = true;
+                break;
+              }
+            }
+          }
+        }
+        if (!cartonExist) {
           qty += detail.cartonQuantity;
-        // }
+        }
       }
     }
     return qty;
