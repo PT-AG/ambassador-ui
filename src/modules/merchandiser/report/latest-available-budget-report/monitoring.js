d9ad00bca6fd5d43debc7fd51752092eea61d153
achensetyawati <achenzetya@gmail.com>
2023-08-28 16:48:41 +0700

    update latest available budget

--- src/modules/merchandiser/report/latest-available-budget-report/monitoring.js
+++ src/modules/merchandiser/report/latest-available-budget-report/monitoring.js
@@ -48,10 +48,10 @@ export class Monitoring {
         this.service.search({ filter: JSON.stringify(this.filter) })
             .then(result => {
                 this.tableData = result.data;
-                // DATA OK LEAD TIME 40  HARI
-                const total35 = this.tableData.filter(f => f.LeadTime == 40).length;
-                const totalOk35 = this.tableData.filter(f => f.DateDiff >= 40 && f.LeadTime == 40).length;
-                const totalNotOk35 = this.tableData.filter(f => f.DateDiff < 40 && f.LeadTime == 40).length;
+                // DATA OK LEAD TIME 35  HARI
+                const total35 = this.tableData.filter(f => f.LeadTime == 35).length;
+                const totalOk35 = this.tableData.filter(f => f.DateDiff >= 35 && f.LeadTime == 35).length;
+                const totalNotOk35 = this.tableData.filter(f => f.DateDiff < 35 && f.LeadTime == 35).length;
 
                 this.dataOk35 = {
                     total: totalOk35,
