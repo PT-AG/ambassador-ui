9311c9202db8be55f167137ae11800f0d103dc1a
achensetyawati <achenzetya@gmail.com>
2023-08-29 11:57:39 +0700

    update laporan kesiapan RO & Budget

--- src/modules/merchandiser/report/latest-available-ro-garment-report/monitoring.js
+++ src/modules/merchandiser/report/latest-available-ro-garment-report/monitoring.js
@@ -49,10 +49,10 @@ export class Monitoring {
             .then(result => {
                 this.tableData = result.data;
                 
-                // DATA OK LEAD TIME 40  HARI
-                const total35 = this.tableData.filter(f => f.LeadTime == 40).length;
-                const totalOk35 = this.tableData.filter(f => f.DateDiff >= 35 && f.LeadTime == 40).length;
-                const totalNotOk35 = this.tableData.filter(f => f.DateDiff < 35 && f.LeadTime == 40).length;
+                // DATA OK LEAD TIME 35  HARI
+                const total35 = this.tableData.filter(f => f.LeadTime == 35).length;
+                const totalOk35 = this.tableData.filter(f => f.DateDiff >= 30 && f.LeadTime == 35).length;
+                const totalNotOk35 = this.tableData.filter(f => f.DateDiff < 30 && f.LeadTime == 35).length;
 
                 this.dataOk35 = {
                     total: totalOk35,
