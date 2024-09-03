3cd61b0264f55a93476cce409b2d60af28b4459d
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-09-03 14:50:32 +0700

    2024/19/Fix/Item-Uom-FinishingInSubcon
    -HardCode Uom Item Finishing In Subcon menjadi PCS

--- src/modules/garment-production/subcon-finishing-in/data-form.js
+++ src/modules/garment-production/subcon-finishing-in/data-form.js
@@ -261,7 +261,12 @@ export class DataForm {
                     if (ccResult.data && ccResult.data[0]) {
                         this.data.Article = ccResult.data[0].Article;
                         this.data.Comodity = ccResult.data[0].Comodity;
-                        this.uom = ccResult.data[0].UOM;
+                        // this.uom = ccResult.data[0].UOM;
+                        this.uom = {
+                            // Id: 12,
+                            Id: 5,
+                            Unit: "PCS"
+                        };
                     }
 
                     this.purchasingService.getGarmentURN(info)
