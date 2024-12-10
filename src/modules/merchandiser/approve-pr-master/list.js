9e0e6cead4e056b272d9972f310c9ff007e39dc8
yokana888 <yokana.adita@outlook.com>
2024-12-10 11:06:16 +0700

    2024/26/Enhance/ChangeMdFilterPRMaster

--- src/modules/merchandiser/approve-pr-master/list.js
+++ src/modules/merchandiser/approve-pr-master/list.js
@@ -173,7 +173,7 @@ export class List {
             case "MD1":
                 this.section = 
                 { 
-                    ApprovalCC: username == "PUSPITA PRIMAHARANI" ? "VERA FATIMAH" : (username == "BAYU SETIAWAN" ? "WAHYU PAMUNGKAS" : username)
+                    ApprovalCC: username == "PUSPITA PRIMAHARANI" ? "VERA FATIMAH" : (username == "SEPTIANA SANDRA" ? "WAHYU PAMUNGKAS" : username)
                 };
                 break;
 
