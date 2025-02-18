217a7d083cbce6a80b8c42d00336812cc6b27561
ardana96 <ardanagama@gmail.com>
2025-02-18 08:45:28 +0700

    Enhancement Rubah Nama VB - SPRINT 4 - 18/02/25

--- src/modules/purchasing/vb-with-po-request/index.js
+++ src/modules/purchasing/vb-with-po-request/index.js
@@ -1,10 +1,15 @@
 export class Index {
   configureRouter(config, router) {
       config.map([
-          { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: Permohonan VB dengan PO' },
-          { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Permohonan VB dengan PO' },
-          { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  Permohonan VB dengan PO' },
-          { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Permohonan VB dengan PO' },
+          // { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: Permohonan VB dengan PO' },
+          // { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: Permohonan VB dengan PO' },
+          // { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  Permohonan VB dengan PO' },
+          // { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: Permohonan VB dengan PO' },
+
+          { route: ['', 'list'], moduleId: './list', name: 'list', nav: false, title: 'List: UANG MUKA PEMBELIAN' },
+          { route: 'create', moduleId: './create', name: 'create', nav: false, title: 'Create: UANG MUKA PEMBELIAN' },
+          { route: 'view/:id', moduleId: './view', name: 'view', nav: false, title: 'View:  UANG MUKA PEMBELIAN' },
+          { route: 'edit/:id', moduleId: './edit', name: 'edit', nav: false, title: 'Edit: UANG MUKA PEMBELIAN' },
       ]);
 
       this.router = router;
