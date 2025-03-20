b8f905639a18e84c984f67329672d01499a4f5ca
ardana96 <ardanagama@gmail.com>
2025-03-20 08:18:07 +0700

    Update Permission VB - SPRINT 6

--- src/modules/expedition/shared/permission-constants.js
+++ src/modules/expedition/shared/permission-constants.js
@@ -1,23 +1,23 @@
 export const VERIFICATION = {
-    key: 'VERIFICATION',
-    label: 'Verifikasi',
-    position: 3, //VERIFICATION_DIVISION
-    positionAutocomplete: 2, //SEND_TO_VERIFICATION_DIVISION
-    code: ['K41'],
+  key: "VERIFICATION",
+  label: "Verifikasi",
+  position: 3, //VERIFICATION_DIVISION
+  positionAutocomplete: 2, //SEND_TO_VERIFICATION_DIVISION
+  code: ["K39"],
 };
 
 export const CASHIER = {
-    key: 'CASHIER',
-    label: 'Kasir',
-    position: 7, //CASHIER_DIVISION
-    positionAutocomplete: 4, //SEND_TO_CASHIER_DIVISION
-    code: ['K39'],
+  key: "CASHIER",
+  label: "Kasir",
+  position: 7, //CASHIER_DIVISION
+  positionAutocomplete: 4, //SEND_TO_CASHIER_DIVISION
+  code: ["K40"],
 };
 
 export const ACCOUNTING = {
-    key: 'ACCOUNTING',
-    label: 'Akuntansi',
-    position: 8, //ACCOUNTING_DIVISION
-    positionAutocomplete: 5, //SEND_TO_ACCOUNTING_DIVISION
-    code: ['K40'],
+  key: "ACCOUNTING",
+  label: "Akuntansi",
+  position: 8, //ACCOUNTING_DIVISION
+  positionAutocomplete: 5, //SEND_TO_ACCOUNTING_DIVISION
+  code: ["K41"],
 };
