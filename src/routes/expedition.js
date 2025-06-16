ea9092f7cc25e21532765270e705f04e33bd1152
ardana96 <ardanagama@gmail.com>
2025-06-16 08:45:28 +0700

    Ubah Nama Menu VB - SPRINT 13

--- src/routes/expedition.js
+++ src/routes/expedition.js
@@ -759,7 +759,7 @@ module.exports = [
     moduleId: "./modules/purchasing/vb-request-document-non-po/index",
     nav: true,
     //title: "Permohonan VB Non PO",
-    title: "Permohonan VB Non PO ( VB Cash )",
+    title: "VB Cash",
     auth: true,
     settings: {
       group: "finance",
@@ -774,7 +774,7 @@ module.exports = [
     name: "vb-with-po-request",
     moduleId: "./modules/purchasing/vb-with-po-request/index",
     nav: true,
-    title: "Permohonan VB Dengan PO (Uang Muka Pembelian)",
+    title: "Uang Muka Pembelian",
     auth: true,
     settings: {
       group: "finance",
@@ -820,7 +820,7 @@ module.exports = [
     moduleId: "./modules/purchasing/realization-vb-non-po/index",
     nav: true,
     //title: "Realisasi VB non PO",
-    title: "Realisasi VB non PO ( VB Cash )",
+    title: "Realisasi VB Cash",
     auth: true,
     settings: {
       group: "finance",
@@ -836,7 +836,7 @@ module.exports = [
     moduleId: "./modules/purchasing/vb-realization-with-po-document/index",
     nav: true,
     //title: "Realisasi VB dengan PO",
-    title: "Realisasi VB dengan PO ( Realisasi Uang Muka Pembelian )",
+    title: "Realisasi Uang Muka Pembelian",
     auth: true,
     settings: {
       group: "finance",
