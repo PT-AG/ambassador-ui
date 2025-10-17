0d9df820cc3046144ebd35f8845061352cd7f8f9
Aji <sarwonoaji27@gmail.com>
2025-10-17 08:01:40 +0700

    AMS - Enhance Surat Jalan

--- src/modules/garment-purchasing/delivery-order/data-form.js
+++ src/modules/garment-purchasing/delivery-order/data-form.js
@@ -11,7 +11,11 @@ export class DataForm {
     @bindable title;
     @bindable supplier;
     @bindable options = {};
-
+    @bindable isView = false;
+    @bindable supplierImport = false;
+    @bindable showHSCodePopup = false;
+    @bindable productHSCodeList = [];
+    @bindable onHSCode = false;
     shipmentTypes = ['By Air', 'By Sea']
     controlOptions = {
         label: {
@@ -38,12 +42,18 @@ export class DataForm {
     bind(context) {
         this.context = context;
         this.data = this.context.data;
-        console.log(this.data.items           );
         this.error = this.context.error;
+        console.log(this.data);
+        //this.options.isImport = this.isImport;
+        this.Import = false;
         var hasItems=true;
         if (this.data.items.length==0)
             hasItems=false;
-        console.log(this.data);
+        // console.log(this.data.supplier.Import);
+        
+        if(this.supplierImport == true){
+            this.supplierImport=true;
+        }
         
         
         // if (this.data.totalQuantity)
@@ -65,7 +75,8 @@ export class DataForm {
                 isEdit: this.isEdit,
                 hasView: this.context.hasView,
                 hasEdit:this.context.hasEdit,
-                hasCreate:this.context.hasCreate
+                hasCreate:this.context.hasCreate,
+                isImport: this.isImport
             }
         } else {
             var filter = {
@@ -80,7 +91,8 @@ export class DataForm {
                 isEdit: this.isEdit,
                 hasView: this.context.hasView,
                 hasEdit:this.context.hasEdit,
-                hasCreate:this.context.hasCreate
+                hasCreate:this.context.hasCreate,
+                supplierImport: this.data.supplier.Import
             }
         }
         return filter;
@@ -142,7 +154,7 @@ export class DataForm {
             }
         }
     }
-
+    
     shipmentTypeChanged(e) {
         var selectedPayment = e.srcElement.value;
         if (selectedPayment) {
@@ -153,7 +165,86 @@ export class DataForm {
     itemChanged(e){
         console.log("after change parent",this);
         console.log("after change parent event",e);
+        this.updateProductHSCodeList();
+        this.onHSCode = true;
     }
+
+    showProductHSCode() {
+        
+        const products = this.data.items.flatMap(item =>
+            item.fulfillments
+            .filter(detail => detail.isSave === true)
+            .map(detail => detail.product)
+        );
+        console.log("All Products:", products);
+        
+        const uniqueProducts = products.filter(
+        (value, index, self) =>
+            index === self.findIndex((t) => t.Code === value.Code && t.Name === value.Name)
+        );
+
+        this.productHSCodeList = uniqueProducts.map(p => ({
+            productName: p.Name+ "-" + p.Code,
+            hsCode: p.HSCode || p.HsCode || "" 
+        }));
+
+        this.showHSCodePopup = true;
+        console.log("Unique Products:", this.productHSCodeList);
+        }
+
+    closePopup(event) {
+    if (!event || event.target.classList.contains('hs-popup-overlay')) {
+        this.showHSCodePopup = false;
+    }
+    }
+
+    saveHSCode() {
+        console.log("Data HS Code disimpan:", this.productHSCodeList);
+
+        this.data.items.forEach(item => {
+            item.fulfillments.forEach(detail => {
+            const match = this.productHSCodeList.find(p => p.productName === detail.product.Name+"-"+detail.product.Code);
+            if (match) {
+                detail.product.HSCode = match.hsCode;
+            }
+            });
+        });
+
+    alert("HS Code berhasil diperbarui!");
+    this.showHSCodePopup = false;
+    console.log("Data setelah menyimpan HS Code:", this.data);
+    }
+
+    updateProductHSCodeList() {
+
+        const products = this.data.items.flatMap(item =>
+        item.fulfillments.map(detail => detail.product)
+        );
+
+        const uniqueProducts = products.filter(
+            (value, index, self) =>
+                index === self.findIndex((t) => t.Code === value.Code && t.Name === value.Name)
+        );
+
+        const oldHSCodeMap = new Map();
+        if (this.productHSCodeList && this.productHSCodeList.length > 0) {
+            this.productHSCodeList.forEach(p => {
+                oldHSCodeMap.set(p.productName, p.hsCode);
+            });
+        }
+
+        this.productHSCodeList = uniqueProducts.map(p => {
+            const key = p.Name + "-" + p.Code;
+            return {
+                productName: key,
+                hsCode: oldHSCodeMap.get(key) || p.HSCode || ""
+            };
+        });
+
+        console.log("Updated HSCode List:", this.productHSCodeList);
+        }
+
+
 
     get deleted() {
         var receiptQty = 0;
@@ -163,13 +254,10 @@ export class DataForm {
             
             for(var detail of item.fulfillments)
             {
-               
                 receiptQty += detail.receiptQuantity;
             }
-           // console.log(receiptQty);
 
         }
-        console.log(this.data.isInvoice);
         return receiptQty !=0 || this.data.isCustoms || this.data.isInvoice;
         //return this.data.isCustoms;
     }
