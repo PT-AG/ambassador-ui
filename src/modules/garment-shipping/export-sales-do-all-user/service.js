4c00971fa9a278308cc0c28b2a6004709ea5ea35
yokana888 <yokana.adita@outlook.com>
2026-01-19 10:30:54 +0700

    2026/1/Enhance/DOSales/R2

--- src/modules/garment-shipping/export-sales-do-all-user/service.js
+++ src/modules/garment-shipping/export-sales-do-all-user/service.js
@@ -2,6 +2,7 @@ import { RestService } from '../../../utils/rest-service';
 
 const serviceUri = 'garment-shipping/export-sales-dos';
 const serviceUriCoverLetter = 'garment-shipping/cover-letters';
+const serviceUriPackingList = 'garment-shipping/packing-lists';
 
 class Service extends RestService {
     constructor(http, aggregator, config, endpoint) {
@@ -15,6 +16,11 @@ class Service extends RestService {
 
     getById(id) {
         var endpoint = `${serviceUri}/${id}`;
+        return super.get(endpoint);
+    }
+
+    getPackingListById(id) {
+        var endpoint = `${serviceUriPackingList}/${id}`;
         return super.get(endpoint);
     }
 
