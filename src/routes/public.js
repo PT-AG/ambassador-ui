8ed53fc98920124669d956e1da4499edf9ee2dae
Theo Samuel Dicunawi Aritonang <theotahu99@gmail.com>
2026-03-30 14:11:43 +0700

    S7-03-26/Fix authentication need for change pass

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,30 +1,30 @@
 module.exports = [
-    {
-        route: 'login',
-        name: 'login',
-        moduleId: './login',
-        nav: true,
-        title: 'login'
-    },
-    {
-        route: 'samples',
-        name: 'samples',
-        moduleId: './samples/index',
-        nav: false,
-        title: 'samples'
-    },
-    {
-        route: 'forbidden',
-        name: 'forbidden',
-        moduleId: './forbidden',
-        nav: false,
-        title: 'forbidden'
-    },
-    {
-        route: 'changepass',
-        name: 'changepass',
-        moduleId: './changepass',
-        nav: false,
-        auth: true,
-        title: 'ChangePass'
-    },]
+  {
+    route: "login",
+    name: "login",
+    moduleId: "./login",
+    nav: true,
+    title: "login",
+  },
+  {
+    route: "samples",
+    name: "samples",
+    moduleId: "./samples/index",
+    nav: false,
+    title: "samples",
+  },
+  {
+    route: "forbidden",
+    name: "forbidden",
+    moduleId: "./forbidden",
+    nav: false,
+    title: "forbidden",
+  },
+  {
+    route: "changepass",
+    name: "changepass",
+    moduleId: "./changepass",
+    nav: false,
+    title: "ChangePass",
+  },
+];
