eff5b66de2ac2635769e8c614d22adcc2347d657
yokana888 <yokana.adita@outlook.com>
2026-05-20 16:41:04 +0700

    2026/11/Enhance/GarmentScrapDO

---
+++ src/modules/garment-production/scrap-delivery-order/edit.js
@@ -0,0 +1,47 @@
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
+
+
+@inject(Router, Service)
+export class Edit {
+    isEdit = true;
+    constructor(router, service) {
+        this.router = router;
+        this.service = service;
+    }
+
+    async activate(params) {
+        var id = params.id;
+        this.data = await this.service.read(id);
+
+    }
+
+    bind(context) {
+        this.context = context;
+        this.data = this.context.data;
+        this.error = this.context.error;
+        this.hasCancel = true;
+        this.hasSave = true;
+        this.isEdit = true;
+    }
+
+    list() {
+        this.router.navigateToRoute('list');
+    }
+
+    cancel(event) {
+        this.list();
+    }
+
+    save(event) {
+        this.service.update(this.data)
+            .then(result => {
+                this.cancel();
+            })
+            .catch(e => {
+                this.error = e;
+            })
+    }
+}
+
