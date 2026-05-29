a74224382df905d91a87dc1518fc61407e31624f
achensetyawati <achenzetya@gmail.com>
2026-05-29 08:14:03 +0700

    update report pebDate

--- src/modules/garment-production/monitoring-expenditure-good-delivery/list.js
+++ src/modules/garment-production/monitoring-expenditure-good-delivery/list.js
@@ -33,7 +33,7 @@ export class List {
                 for(var _data of result){
                       
                     _data.expenditureDate= moment(_data.expenditureDate).format("YYYY-MM-DD");
-                    _data.pebDate=  moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1970" || moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1900" ? "-" : moment(_data.pebDate).format("YYYY-MM-DD");
+                    //_data.pebDate=  moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1970" || moment(_data.pebDate).format("DD MMM YYYY") == "01 Jan 1900" ? "-" : moment(_data.pebDate).format("YYYY-MM-DD");
                     _data.price=_data.price.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                     _data.qtys=_data.qty.toLocaleString('en-EN', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
                     this.data.push(_data);
