b7ccc10b3fda255e9895cd39f9dd08d8c970f7bc
dwinainggolan09 <dwinainggolan09@gmail.com>
2026-07-16 16:46:57 +0700

    16/07/2026 masking inventory

--- src/modules/inventory/garment-leftover-warehouse/master/comodity/view.js
+++ src/modules/inventory/garment-leftover-warehouse/master/comodity/view.js
@@ -1,6 +1,7 @@
 import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Service } from './service';
+import { Base64Helper } from '../../../../../utils/base-64-coded-helper';
 
 @inject(Router, Service)
 export class View {
@@ -10,7 +11,8 @@ export class View {
     }
 
     async activate(params) {
-        var id = params.id;
+        const decoded = Base64Helper.decode(params.id);
+        var id = decoded;
         this.data = await this.service.getById(id);
     }
 
@@ -23,7 +25,8 @@ export class View {
     }
 
     editCallback(event) {
-        this.router.navigateToRoute('edit', { id: this.data.Id });
+        const encoded = Base64Helper.encode(this.data.Id);
+        this.router.navigateToRoute('edit', { id: encoded });
     }
 
     deleteCallback(event) {
