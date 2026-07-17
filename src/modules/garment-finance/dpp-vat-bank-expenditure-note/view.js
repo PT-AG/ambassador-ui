f0c557a91183b95f1e949acaa8eaf83c39ed99dc
dwinainggolan09 <dwinainggolan09@gmail.com>
2026-07-17 10:33:02 +0700

    17/07/2026 masking G-FINANCE

--- src/modules/garment-finance/dpp-vat-bank-expenditure-note/view.js
+++ src/modules/garment-finance/dpp-vat-bank-expenditure-note/view.js
@@ -2,6 +2,7 @@ import { inject, Lazy } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { Service } from './service';
 import { Dialog } from '../../../au-components/dialog/dialog';
+import { Base64Helper } from "../../../utils/base-64-coded-helper";
 
 @inject(Router, Service, Dialog)
 export class View {
@@ -12,7 +13,8 @@ export class View {
     }
 
     async activate(params) {
-        let id = params.id;
+    	const decoded = Base64Helper.decode(params.id);
+    	let id = decoded;
         this.data = await this.service.getById(id);
         // this.data.IsPosted = true;
 
@@ -31,7 +33,8 @@ export class View {
     }
 
     editCallback(event) {
-        this.router.navigateToRoute('edit', { id: this.data.Id });
+            const encoded = Base64Helper.encode(this.data.Id);
+            this.router.navigateToRoute('edit', { id: encoded });
     }
 
     postingCallback(event) {
--- src/modules/garment-finance/garment-disposition-verification/view.js
+++ src/modules/garment-finance/garment-disposition-verification/view.js
@@ -1,94 +1,113 @@
-import { inject, Lazy, bindable } from 'aurelia-framework';
-import { Router } from 'aurelia-router';
-import { Service } from './service';
-import { activationStrategy } from 'aurelia-router';
+import { inject, Lazy, bindable } from "aurelia-framework";
+import { Router } from "aurelia-router";
+import { Service } from "./service";
+import { activationStrategy } from "aurelia-router";
 
-import { Dialog } from '../../../components/dialog/dialog';
-const ExpeditionLoader = require('../shared/disposition-expedition-loader');
-import { CreateSubmit } from './dialog-template/create-submit';
-import { PurchasingService } from '../shared/purchasing-service';
+import { Dialog } from "../../../components/dialog/dialog";
+const ExpeditionLoader = require("../shared/disposition-expedition-loader");
+import { CreateSubmit } from "./dialog-template/create-submit";
+import { PurchasingService } from "../shared/purchasing-service";
+import { Base64Helper } from "../../../utils/base-64-coded-helper";
 
 @inject(Router, Service, Dialog, PurchasingService)
 export class View {
-    controlOptions = {
-        label: {
-            length: 4,
-        },
-        control: {
-            length: 4,
-        },
-    };
+  controlOptions = {
+    label: {
+      length: 4,
+    },
+    control: {
+      length: 4,
+    },
+  };
 
-    formOptions = {
-        cancelText: 'Kembali',
-        saveText: 'Verifikasi',
-    };
+  formOptions = {
+    cancelText: "Kembali",
+    saveText: "Verifikasi",
+  };
 
-    constructor(router, service, dialog, purchasingService) {
-        this.router = router;
-        this.service = service;
-        this.dialog = dialog;
-        this.purchasingService = purchasingService;
-        this.submitContext = {};
-        this.data = {};
-        this.error = {};
-        this.verificationDate = new Date();
+  constructor(router, service, dialog, purchasingService) {
+    this.router = router;
+    this.service = service;
+    this.dialog = dialog;
+    this.purchasingService = purchasingService;
+    this.submitContext = {};
+    this.data = {};
+    this.error = {};
+    this.verificationDate = new Date();
 
-        // this.collection = {
-        //     columns: ['No. Nota Intern', 'Tanggal Nota Intern', 'Tanggal Jatuh Tempo', 'Supplier', 'PPN', 'PPh', 'Total Bayar', 'Mata Uang', 'Keterangan'],
-        //     onAdd: () => {
-        //         this.data.Items.push({});
-        //     },
-        // };
+    // this.collection = {
+    //     columns: ['No. Nota Intern', 'Tanggal Nota Intern', 'Tanggal Jatuh Tempo', 'Supplier', 'PPN', 'PPh', 'Total Bayar', 'Mata Uang', 'Keterangan'],
+    //     onAdd: () => {
+    //         this.data.Items.push({});
+    //     },
+    // };
+  }
+
+  async activate(params) {
+    console.log(params);
+
+    const decoded = Base64Helper.decode(params.id);
+    const id = decoded;
+
+    this.selectedExpedition = await this.service.getById(id);
+    if (this.selectedExpedition) {
+      this.verificationDate = this.selectedExpedition.VerificationAcceptedDate;
+
+      let dispositionNote =
+        await this.purchasingService.getDispositionNoteByIdVerifiedNotCalculated(
+          this.selectedExpedition.DispositionNoteId,
+        );
+      this.dispositionNote = dispositionNote;
+      this.items = dispositionNote.Items;
     }
+  }
 
-    async activate(params) {
-        console.log(params);
+  collection = {
+    columns: [
+      "Nomor External PO",
+      "Kena PPN",
+      "Nominal PPN",
+      "Kena PPh",
+      "PPh",
+      "Nominal PPh",
+      "Verified Amount",
+      "",
+    ],
+    onAdd: function () {
+      // this.context.ItemsCollection.bind();
+      // this.data.items.push({});
+    }.bind(this),
+  };
 
-        this.selectedExpedition = await this.service.getById(params.id);
-        if (this.selectedExpedition) {
-            this.verificationDate = this.selectedExpedition.VerificationAcceptedDate;
+  determineActivationStrategy() {
+    return activationStrategy.replace;
+  }
 
-            let dispositionNote = await this.purchasingService.getDispositionNoteByIdVerifiedNotCalculated(this.selectedExpedition.DispositionNoteId);
-            this.dispositionNote = dispositionNote;
-            this.items = dispositionNote.Items
-        }
+  cancelCallback(event) {
+    this.router.navigateToRoute("list");
+  }
+
+  get expeditionLoader() {
+    return ExpeditionLoader;
+  }
+
+  @bindable selectedExpedition;
+  async selectedExpeditionChanged(newValue, oldValue) {
+    console.log(this);
+
+    if (newValue) {
+      var dispositionNote = await this.purchasingService.getDispositionNoteById(
+        newValue.DispositionNoteId,
+      );
+      console.log(dispositionNote);
+      this.items = dispositionNote.items;
+    } else {
+      this.items = [];
     }
+  }
 
-    collection = {
-        columns: ["Nomor External PO", "Kena PPN", "Nominal PPN", "Kena PPh", "PPh", "Nominal PPh", "Verified Amount", ""],
-        onAdd: function () {
-            // this.context.ItemsCollection.bind();
-            // this.data.items.push({});
-        }.bind(this),
-    };
-
-    determineActivationStrategy() {
-        return activationStrategy.replace;
-    }
-
-    cancelCallback(event) {
-        this.router.navigateToRoute('list');
-    }
-
-    get expeditionLoader() {
-        return ExpeditionLoader;
-    }
-
-    @bindable selectedExpedition;
-    async selectedExpeditionChanged(newValue, oldValue) {
-        console.log(this);
-
-        if (newValue) {
-            var dispositionNote = await this.purchasingService.getDispositionNoteById(newValue.DispositionNoteId);
-            console.log(dispositionNote);
-            this.items = dispositionNote.items;
-        } else {
-            this.items = [];
-        }
-    }
-
-    editCallback(event) {
-        this.router.navigateToRoute('edit', { id: this.selectedExpedition.Id });
-    }
+  editCallback(event) {
+    const encoded = Base64Helper.encode(this.selectedExpedition.Id);
+    this.router.navigateToRoute("edit", { id: encoded });
+  }
 }
