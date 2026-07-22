8a4d76af03b600a077e1790c3385b14bc096ea86
Theo Samuel Dicunawi Aritonang <theotahu99@gmail.com>
2026-07-22 10:16:13 +0700

    S15/07-26/g-purchasing, add buyer dropdown filter

--- src/modules/garment-purchasing/monitoring-purchase-order/service.js
+++ src/modules/garment-purchasing/monitoring-purchase-order/service.js
@@ -17,8 +17,8 @@ export class Service extends RestService {
         return super.list(endpoint, args);
     }
 
-    generateExcel(epono, unit, roNo, article, poSerialNumber, doNo, ipoStatus, supplier, status, dateFrom, dateTo, dateFromEx, dateToEx, shipmentDateFrom, shipmentDateTo) {
-        var endpoint = `${serviceUri}/download?unit=${unit}&article=${article}&poSerialNumber=${poSerialNumber}&ipoStatus=${ipoStatus}&epono=${epono}&roNo=${roNo}&doNo=${doNo}&supplier=${supplier}&dateFrom=${dateFrom}&dateTo=${dateTo}&dateFromEx=${dateFromEx}&dateToEx=${dateToEx}&status=${status}&shipmentDateFrom=${shipmentDateFrom}&shipmentDateTo=${shipmentDateTo}`;
+    generateExcel(epono, unit, roNo, article, poSerialNumber, doNo, ipoStatus, supplier, buyer, status, dateFrom, dateTo, dateFromEx, dateToEx, shipmentDateFrom, shipmentDateTo) {
+        var endpoint = `${serviceUri}/download?unit=${unit}&article=${article}&poSerialNumber=${poSerialNumber}&ipoStatus=${ipoStatus}&epono=${epono}&roNo=${roNo}&doNo=${doNo}&supplier=${supplier}&buyer=${buyer}&dateFrom=${dateFrom}&dateTo=${dateTo}&dateFromEx=${dateFromEx}&dateToEx=${dateToEx}&status=${status}&shipmentDateFrom=${shipmentDateFrom}&shipmentDateTo=${shipmentDateTo}`;
         console.log(doNo);
         console.log(endpoint);
         return super.getXls(endpoint);
