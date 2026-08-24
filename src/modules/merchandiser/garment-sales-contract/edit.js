68bccd8408b1dfbcd1dc37c6da41c60039be98ad
Theo Samuel Dicunawi Aritonang <theotahu99@gmail.com>
2026-08-24 08:43:41 +0700

    S17/08-26/fix SC edit module

--- src/modules/merchandiser/garment-sales-contract/edit.js
+++ src/modules/merchandiser/garment-sales-contract/edit.js
@@ -1,59 +1,39 @@
-import {inject, Lazy} from 'aurelia-framework';
-import {Router} from 'aurelia-router';
-import {Service} from './service';
+import { inject, Lazy } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Service } from './service';
 import { Base64Helper } from '../../../utils/base-64-coded-helper';
 
-
 @inject(Router, Service)
-export class View {
-    hasEdit = true;
-    hasDelete = true;
-
+export class Edit {
     constructor(router, service) {
         this.router = router;
         this.service = service;
     }
+    isEdit = true;
 
-    async activate(params, routeConfig, navigationInstruction) {
-        const instruction = navigationInstruction.getAllInstructions()[0];
-        const parentInstruction = instruction.parentInstruction;
-        const byUser = parentInstruction.config.settings.byUser;
-
-        const decoded = Base64Helper.decode(params.id);
-        var id = decoded;
-
+    async activate(params) {
+        var id = Base64Helper.decode(params.id);
         this.data = await this.service.getById(id);
-        if (this.data && this.data.CostCalculationId) {
-            let costCal = await this.service.getCostCalById(this.data.CostCalculationId);
-            if (costCal.RO_GarmentId) {
-                this.hasEdit = false;
-                this.hasDelete = false;
+    }
+
+    view(data) {
+        const encoded = Base64Helper.encode(this.data.Id);
+        this.router.navigateToRoute('view', { id: encoded });
+    }
+
+    save() {
+        if(this.data.SalesContractROs){
+            for(var item of this.data.SalesContractROs){
+                if(item.Items && item.Items.length===0){
+                    item.Price=item.Price?item.Price:0;
+                }
             }
         }
-        this.hasItems=true;
-
-        if (!byUser) {
-            this.hasEdit = false;
-            this.hasDelete = false;
-        }
-    }
-
-    list() {
-        this.router.navigateToRoute('list');
-    }
-
-    edit(data) {
-      const encoded = Base64Helper.encode(this.data.Id);
-      this.router.navigateToRoute('view', { id: encoded });
-      //this.router.navigateToRoute('view', { id: this.data.Id });
-    }
-
-    delete() {
-        if (confirm("Delete?")) {
-            this.service.delete(this.data)
-                .then(result => {
-                    this.list();
-                });
-        }
+        
+        this.service.update(this.data).then(result => {
+            this.view();
+        }).catch(e => {
+            this.error = e;
+        })
     }
 }
