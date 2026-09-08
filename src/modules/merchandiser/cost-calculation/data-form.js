2246424fbb5eff3fb80185d8ed25c0b97b5856b7
achensetyawati <achen.setyawati@gmail.com>
2026-09-08 08:39:56 +0700

    8/9/26/ change PRMaster filter to buyer

--- src/modules/merchandiser/cost-calculation/data-form.js
+++ src/modules/merchandiser/cost-calculation/data-form.js
@@ -260,7 +260,7 @@ export class DataForm {
     }
 
     this.costCalculationGarment_MaterialsInfo.options.CCId = this.data.Id;
-    this.costCalculationGarment_MaterialsInfo.options.SCId = this.data.PreSCId;
+    this.costCalculationGarment_MaterialsInfo.options.BuyerId = this.data.Buyer.Id;
 
     if (this.data.Rate) {
       if (this.data.Rate.Value > 1) {
@@ -391,7 +391,7 @@ export class DataForm {
         materialFromPRmaster.POMaster = null;
       }
     }
-    this.costCalculationGarment_MaterialsInfo.options.SCId = this.data.PreSCId;
+    this.costCalculationGarment_MaterialsInfo.options.BuyerId = this.data.Buyer.Id;
   }
 
 
