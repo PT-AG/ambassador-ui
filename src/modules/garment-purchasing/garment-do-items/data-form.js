ffe68cade3e06e3023ae93e4eaf75b122adc01a1
achensetyawati <achen.setyawati@gmail.com>
2026-09-15 10:13:37 +0700

    15/9/26/ update racking info

--- src/modules/garment-purchasing/garment-do-items/data-form.js
+++ src/modules/garment-purchasing/garment-do-items/data-form.js
@@ -1,4 +1,5 @@
 import { inject, bindable, computedFrom,BindingEngine } from 'aurelia-framework';
+import moment from "moment";
 
 @inject(BindingEngine)
 export class DataForm {
@@ -29,32 +30,76 @@ export class DataForm {
     }
   }
 
-  columns= [
-    "Warna",
-    "Quantity",
-    "Rack",
-    "Level",
-    "Box",
-    "Area",
-];
+//   columns= [
+//     "Warna",
+//     "Quantity",
+//     "Rack",
+//     "Level",
+//     "Box",
+//     "Area",
+// ];
 
   bind(context) {
    
     this.context = context;
     this.data = this.context.data;
     
+    this.isFabric =
+      this.data &&
+      this.data.ProductName &&
+      this.data.ProductName.trim().toUpperCase() === "FABRIC";
+
+    this.columns = this.isFabric
+      ? [
+          "Warna",
+          "Quantity",
+          "Lot",
+          "Batch",
+          "No Package",
+          "Handling Unit",
+          "Rack",
+          "Level",
+          "Box",
+          "Area",
+        ]
+      : [
+          "Warna",
+          "Quantity",
+          "Batch",
+          "No Package",
+          "Handling Unit",
+          "Rack",
+          "Level",
+          "Box",
+          "Area",
+        ];
 
     //binding Items from header
     this.data.Items=[];
     if(this.data)
     {
       var item ={};
+      item.ProductName = this.data.ProductName;
       item.Rack = this.data.Rack;
       item.Box = this.data.Box;
       item.Colour = this.data.Colour;
       item.Level = this.data.Level;
       item.Area = this.data.Area;
       item.Quantity = this.data.RemainingQuantity;
+      item.Lot = this.data.Lot;
+
+      item.Batch = this.data.Batch
+        ? moment.parseZone(this.data.Batch)
+            .utcOffset(7)
+            .format("YYYY-MM-DD")
+        : moment.parseZone(new Date())
+            .utcOffset(7)
+            .format("YYYY-MM-DD");
+
+      item.HandlingUnitId = this.data.HandlingUnitId;
+      item.HandlingUnit = this.data.HandlingUnit;
+      item.PackageNo = this.data.PackageNo;
+
       this.data.Items.push(item);
     }
 
@@ -79,10 +124,51 @@ export class DataForm {
   }
 
   get addItems() {
-    return (event) => {
-      this.data.Items.push({});
-    };
-  }
+  return (event) => {
+    const lastItem = this.data.Items && this.data.Items.length > 0
+      ? this.data.Items[this.data.Items.length - 1]
+      : null;
+
+    if (lastItem) {
+
+      this.data.Items.push({
+        ProductName: lastItem.ProductName,
+        Colour: lastItem.Colour,
+        Quantity: 0,
+        Lot: lastItem.Lot,
+        Batch: lastItem.Batch
+          ? moment.parseZone(lastItem.Batch).utcOffset(7).format("YYYY-MM-DD")
+          : null,
+        NoPackage: lastItem.NoPackage,
+        HandlingUnitId: lastItem.HandlingUnitId,
+        HandlingUnit: lastItem.HandlingUnit,
+        Rack: lastItem.Rack,
+        Level: lastItem.Level,
+        Box: lastItem.Box,
+        Area: lastItem.Area
+      });
+
+    } else {
+
+      this.data.Items.push({
+        ProductName: this.data.ProductName,
+        Colour: this.data.Colour,
+        Quantity: 0,
+        Lot: this.data.Lot,
+        Batch: this.data.Batch
+          ? moment.parseZone(this.data.Batch).utcOffset(7).format("YYYY-MM-DD")
+          : null,
+        NoPackage: this.data.NoPackage,
+        HandlingUnitId: this.data.HandlingUnitId,
+        HandlingUnit: this.data.HandlingUnit,
+        Rack: this.data.Rack,
+        Level: this.data.Level,
+        Box: this.data.Box,
+        Area: this.data.Area
+      });
+    }
+  };
+}
 
   get removeItems() {
     return (event) => {
